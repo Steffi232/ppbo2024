ece3297a1fec267718b669c82d232993139b04d0
Steffi Then <thensteffi@gmail.com>
2024-09-01 22:49:23 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -5,6 +5,10 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI*$this->jari_jari*$this->jari_jari;
     }
@@ -18,6 +22,11 @@ class Bola
 {
     const PHI = 3.14;
     public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
@@ -28,6 +37,12 @@ class Tabung
     const PHI = 3.14;
     public $jari_jari;
     public $tinggi;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
@@ -39,28 +54,27 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
-$nasi_tumpeng = new Kerucut();
-$nasi_tumpeng->jari_jari = 4;
-$nasi_tumpeng->tinggi = 10;
+$nasi_tumpeng = new Kerucut(4,10);
 echo "Volume nasi tumpeng tersebut adalah " . $nasi_tumpeng->volume();
 
-$lapangan = new Lingkaran();
-$lapangan->jari_jari = 25;
+$lapangan = new Lingkaran(25);
 echo "\nLuas keliling lapagan tersebut adalah " . $lapangan->luas();
 echo "\nKeliling lapangan tersebut adalah " . $lapangan->keliling();
 
-$bola_basket = new Bola();
-$bola_basket->jari_jari = 15;
+$bola_basket = new Bola(15);
 echo "\nVolume bola baket tersebut adalah " . $bola_basket->volume();
 
 $botol = new Tabung(24,30);
-$botol->jari_jari = 24;
-$botol->tinggi = 30;
 echo "\nVolume botol minum tersebut adalah " . $botol->volume();
 
 ?>
