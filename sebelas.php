e8166c57000e7746d9031709701229a7c7793eb2
Steffi Then <thensteffi@gmail.com>
2024-09-20 14:48:57 +0700

    Praktikum 6F - Membuat Objek Pewarisan

--- sebelas.php
+++ sebelas.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'App/Admin/Dosen.php';
-require_once 'App/Admin/TenagaKependidikan.php';
 
 $dian = new \App\Admin\Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
 $dian->mengajar(); 
