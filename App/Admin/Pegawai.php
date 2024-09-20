8f7c3b0c95da5a3f0f18474995c6a1d559fd1323
Steffi Then <thensteffi@gmail.com>
2024-09-20 14:18:41 +0700

    Praktikum 6E - Menggunakan Pewarisan

---
+++ App/Admin/Pegawai.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Admin;
+
+class Pegawai 
+{
+  public $nip;
+  public $nama;
+  protected $no_hp;
+  public $alamat;
+
+  public function __construct($nip, $nama, $no_hp, $alamat) 
+  {
+    $this->nip = $nip;
+    $this->nama = $nama;
+    $this->no_hp = $no_hp;
+    $this->alamat = $alamat;
+  }
+
+  public function cekin() 
+  {
+    return true;
+  }
+
+  public function cekOut() 
+  {
+    return true;
+  }
+
+  public function getNoHp() 
+  {
+    return $this->no_hp;
+  }
+
+  public function setNoHp($newNoHp) 
+  {
+    $this->no_hp = $newNoHp;
+    echo "Nomor Telepon diganti menjadi: " . $this->no_hp; 
+  }
+}
+
+?>
---
+++ App/Admin/TenagaKependidikan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Admin;
+
+require_once 'Pegawai.php';
+
+class TenagaKependidikan extends Pegawai 
+{
+    public $gaji_pokok;
+  
+    public function __construct($nip, $nama, $no_hp, $alamat, $gaji_pokok) 
+    {
+      parent::__construct($nip, $nama, $no_hp, $alamat);
+      $this->gaji_pokok = $gaji_pokok;
+    }
+  
+    public function cuti() 
+    {
+      echo $this->nama . " memutuskan untuk mengambil cuti.";
+    }
+  }
