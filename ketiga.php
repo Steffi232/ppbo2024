5540f417dc06069941e005a42e958786900ebd21
Steffi Then <thensteffi@gmail.com>
2024-09-23 21:01:02 +0700

    Praktikum 4C - Membuat Constructor

---
+++ ketiga.php
@@ -0,0 +1,80 @@
+<?php
+
+class Lingkaran
+{
+    const PHI = 3.14;
+    public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
+    public function luas() : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
+    }
+
+    public function keliling() : float {
+        return 2*self::PHI*$this->jari_jari;
+    }
+}
+
+class Bola
+{
+    const PHI = 3.14;
+    public $jari_jari;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
+    public function volume() : float {
+        return (4/3)*self::PHI*pow($this->jari_jari,3);
+    }
+}
+
+class Tabung
+{
+    const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume() : float {
+        return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
+    }
+}
+
+class Kerucut
+{
+    const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume() : float {
+        return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
+    }
+}
+
+$nasi_tumpeng = new Kerucut(4,10);
+echo "Volume nasi tumpeng tersebut adalah " . $nasi_tumpeng->volume();
+
+$lapangan = new Lingkaran(25);
+echo "\nLuas keliling lapagan tersebut adalah " . $lapangan->luas();
+echo "\nKeliling lapangan tersebut adalah " . $lapangan->keliling();
+
+$bola_basket = new Bola(15);
+echo "\nVolume bola baket tersebut adalah " . $bola_basket->volume();
+
+$botol = new Tabung(24,30);
+echo "\nVolume botol minum tersebut adalah " . $botol->volume();
+
+?>
